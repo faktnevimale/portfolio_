e4297c0c0361b598746070b45ca23d18bb63abab
Barbora Šťávová <barbora.stavova@outlook.cz>
2024-12-08 23:35:01 +0100

    nevimuz

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,11 +7,11 @@ const About: React.FC = () => {
       <p>
         Ahoj! Jmenuju se Barbora a je mi 18 let. Jsem studentkou 3. ročníku IT na SŠ Baltaci ve Zlíně. 
       </p>
-      <h1>Zájmy</h1>
+      <h2>Zájmy</h2>
       <p>
         Kromě programování a grafiky ráda čtu knihy, kreslím a hraju pc hry.
       </p>
-      <h1>Dovednosti</h1>
+      <h2>Dovednosti</h2>
       <ul>
         <li>Grafický design</li>
         <li>Focení</li>
@@ -19,7 +19,7 @@ const About: React.FC = () => {
         <li>Základy JavaScriptu a Reactu</li>
         <li>C#</li>
       </ul>
-      <h1>Budoucnost</h1>
+      <h2>Budoucnost</h2>
       <p>
         Po dokončení školy bych se ráda zaměřila na práci v oblasti grafického designu. Chci se podílet na projektech, které budou mít pozitivní dopad na společnost.
       </p>
