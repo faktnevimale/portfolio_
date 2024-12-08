e0d3a46e2cfada3da3f71efaced1903f29b218dd
Barbora Šťávová <barbora.stavova@outlook.cz>
2024-12-08 23:31:17 +0100

    nevim

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Homepage: React.FC = () => {
     <div className="homepage-container">
       <div className="homepage-text">
         <h1>Vítejte na mém webu!</h1>
-        <p>Jmenuju se Barbora a nabízím 2D a 3D grafiky na zakázku.</p>
+        <p>Jmenuju se Barbora a nabízím 2D a 3D grafiku na zakázku.</p>
       </div>
       <div className="homepage-image">
         <img src={image} alt="Ukázka grafiky" />
